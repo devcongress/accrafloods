79ec2aacd30af0f4ef5e8883a01d1817d51adf37
Claude Ayitey <clauditey@gmail.com>
2015-06-05 02:40:11 +0000

    able to add, edit and delete information about missing persons.

--- Gemfile
+++ Gemfile
@@ -1,7 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'rails', '4.2.1'
-gem 'pg'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
@@ -12,12 +11,13 @@ gem 'haml', '~> 4.0.6'
 gem 'bootstrap-sass', '~> 3.3.4.1'
 gem 'simple_form', '~> 3.1.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'spring',        group: :development
+
 
 group :development, :test do
-  gem 'sqlite3'
+  gem 'sqlite3', '~> 1.3.10'
   gem 'byebug'
   gem 'web-console', '~> 2.0'
-  gem 'spring'
 end
 
 
--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,6 +1,53 @@
 class PeopleController < ApplicationController
 
-    def index
+    before_action :find_person, only: [:show, :edit, :update, :destroy]
+
+    def show
         
+    end
+
+    def index
+        @people = Person.all.order("created_at DESC")
+    end
+
+    def new
+        @person = Person.new
+    end
+
+    def create
+        @person = Person.new(person_params)
+
+        if @person.save
+            redirect_to @person, notice: "Successfully added missing person."
+        else
+            render 'new'
+        end
+    end
+
+    def edit
+        
+    end
+
+    def update
+        if @person.update(person_params)
+            redirect_to @person, notice: "Person's details have been updated."
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @person.destroy
+        redirect_to root_path
+    end
+
+    private
+
+    def person_params
+        params.require(:person).permit(:title, :description)
+    end
+
+    def find_person
+        @person = Person.find(params[:id]) 
     end
 end
